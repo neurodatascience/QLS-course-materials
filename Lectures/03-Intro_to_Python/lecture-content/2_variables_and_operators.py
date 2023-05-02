bec037e7607a3c0aed831743783ed500a5a976e9
Michelle Wang <tomichellewang@gmail.com>
2023-05-01 22:15:51 -0400

    add .py files for Intro to Python

---
+++ Lectures/03-Intro_to_Python/lecture-content/2_variables_and_operators.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+
+# ## variables
+# age = 12 # use '=' to assign a value to a variable
+# print(age)
+
+# age = 24 # update the age variable
+# print(age)
+
+# age = age + 2
+# print(age)
+
+# age += 1 # add 1 to the value of 'age' and assign it back to 'age'
+# print(age)
+
+
+# ## arithmetic operators
+# a = 12
+# b = 2
+# c = 4
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a/b)
+# print(a**b)
+# print(a%b)
+
+
+# ## logical operators
+# x = True
+# y = False
+# z = False
+
+# print(x and y)
+# print(x or y)
+# print(not x)
+# print(x or y or z)
+
+
+# ## comparison operators
+# x = 1
+# y = 1
+# z = 547
+# a = "hello"
+
+# print(x == y) # Equal to
+# print(x == a)
+# print(x != z) # Not equal to
+# print(x > y)  # Greater than
+# print(x >= y) # Greater than or equal to
+
+
+# ## assignment vs equality
+# a = 5
+# a = 4 # assign the value 4 to the variable 'a'
+# print(a)
+
+# a = 5
+# print(a == 4) # check equality
+
+
+# ## operator precedence
+# print(a + c / b)
+# print((a + c) / b)
+
+
+# ## operators and variable types
+# print(True > 'abc')
+# print('qlsc ' + 612)
+
+
+# ## operator overloading
+# print(123 + 489)       # integers
+# print(123. + 489.)     # floats
+# print('qlsc ' + '612') # strings
+
+
+# ## typecasting
+# print(123 + 489.)         # implicit typecasting (note that the output is a float)
+# print('qlsc ' + str(612)) # explicit typecasting
+# # casting string to int
+# print(int('612'))
+# print(int('qlsc')) # invalid
+
+# ## strings
+# message = "Hello, I am a string"
+# print(message)
