95f838be31fe1cb2011c518a39cc16904b40bb37
Remi Gau <remi_gau@hotmail.com>
2024-04-24 13:08:05 +0200

    add data

--- Lectures/2022/07-Machine_Learning_1/lecture-content/env-setup_test.py
+++ Lectures/2022/07-Machine_Learning_1/lecture-content/env-setup_test.py
@@ -7,13 +7,7 @@ import pandas as pd
 import seaborn as sns
 from nilearn import datasets, plotting
 from nilearn.connectome import ConnectivityMeasure
-from sklearn import (
-    datasets,
-    linear_model,
-    metrics,
-    model_selection,
-    preprocessing,
-)
+from sklearn import datasets, linear_model, metrics, model_selection, preprocessing
 from sklearn.base import clone
 from sklearn.dummy import DummyRegressor
 from sklearn.ensemble import RandomForestClassifier
