25c8e782d70f7ae5032fb6e8f5a3c72b04ed2a4c
julia-pfarr <juliakatharina.pfarr@gmx.de>
2026-04-17 14:06:00 -0400

    prepare for adding submodule

--- Lectures/2025/02_terminal-bash/shell-course/interesting_files/i_can_see_variables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Print a message for two variables that could be passed in the environment
-echo I can see this environment variable: ${ENV_VAR}. Very good
-echo I cannot see this shell variable: ${shell_var}. How strange
--- Lectures/2025/02_terminal-bash/shell-course/interesting_files/run_me.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# The following command will print a message
-echo "Thank you, very kind!"
