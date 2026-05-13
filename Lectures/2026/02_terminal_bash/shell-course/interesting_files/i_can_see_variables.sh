cd4aeed2e4f55f8ddcc9c68cece05ecb379981cf
Alyssa Dai <alyssa.ydai@gmail.com>
2026-05-12 23:09:04 -0400

    [ENH] Add installation clinic slides and bash module materials (#68)
    * add slides
    
    * remove README
    
    * add installation clinic slides
    
    * add shell-course example files
    
    * add bash exercises
    
    * add image for exercises
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ Lectures/2026/02_terminal_bash/shell-course/interesting_files/i_can_see_variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Print a message for two variables that could be passed in the environment
+echo I can see this environment variable: ${ENV_VAR}. Very good
+echo I cannot see this shell variable: ${shell_var}. How strange
